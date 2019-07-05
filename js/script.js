6cfa3d5f4a2344ad1a1bd2b2303b4729e3ba3db7
pateskinasy <pateskinasy@users.noreply.github.com>
2019-07-05 16:57:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 //fetching from json
 fetch('/random-quotes-vanillajs-json/data/quotes.json') // Call the fetch function passing the url of the API as a parameter
 .then(response =>  {
-    response.json() // Your code for handling the data you get from the API
+    console.log(response.json) // Your code for handling the data you get from the API
 });
 // .catch(function() {
 //     // This is where you run code if the server returns any errors
