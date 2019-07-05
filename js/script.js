f2413bcf8f8c75c9b5d81eda4947347f70f573be
pateskinasy <pateskinasy@users.noreply.github.com>
2019-07-05 17:14:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,11 @@
 //fetching from json
 fetch('/random-quotes-vanillajs-json/data/quotes.json') // Call the fetch function passing the url of the API as a parameter
 .then(response =>  {
-    console.log(response.json())    
+    console.log(response)    
 });
+// .then(response =>  {
+//     console.log(response.json())    
+// });
 
 
 //function to access random quote from array and inject it together with author on HTML
