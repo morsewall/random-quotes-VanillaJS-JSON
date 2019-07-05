55d3a0a7df6628da3fab166f737953aecc8cc550
pateskinasy <pateskinasy@users.noreply.github.com>
2019-07-05 18:08:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 //fetching data from JSON file
 const quotes = fetch('/random-quotes-vanillajs-json/data/quotes.json') // Call the fetch function passing the url of the API as a parameter
 .then(response => response.json())    
-.then(responseJSON => console.log(responseJSON));
+// .then(responseJSON => console.log(responseJSON));
 
 //convert JSON data to an array with the quotes
 // const quotes = JSON.parse(request);
