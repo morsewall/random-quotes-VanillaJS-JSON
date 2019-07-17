bf08e32833e18a48133e97ed0b1a8768191133fe
pateskinasy <pateskinasy@users.noreply.github.com>
2019-07-17 12:38:14 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,18 +20,26 @@ function injectQuote() {
   //inject author on HTML
   document.getElementById('author').innerHTML = "- " + randomQuote.quoteAuthor;
   let quoteAuthorElem = " - " + randomQuote.quoteAuthor;
+  const lineBreak = "%0a";
+  const related = "&related=celue";
+  const via = "&via=morsewall";
+  const addOnHandle = " @celue";
   //truncating quote text in case full tweet gets to be over 280 characters
-  let contentQuote = quoteTextElem + quoteAuthorElem;
+  let contentQuote = quoteTextElem + quoteAuthorElem + lineBreak + via + addOnHandle;
   if (contentQuote.length > 280) {
     let charCountAuthor = quoteAuthorElem.length;
+    let viaCharCount = via.length;
+    let addOnHandleCharCount = addOnHandle.length;
     const extraStylingChar = "..." + '"';
-    let extraCharCount = extraStylingChar.length;
-    let subString = quoteTextElem.substring(0, 280 - extraCharCount - charCountAuthor) + extraStylingChar + quoteAuthorElem;
+    const extraCharCount = extraStylingChar.length;
+    const tweetAdjustmentChar = "  ";
+    const tweetAdjustmentCharCount = tweetAdjustmentChar.length;
+    let subString = quoteTextElem.substring(0, 280 - extraCharCount - charCountAuthor - viaCharCount - addOnHandleCharCount - tweetAdjustmentCharCount) + extraStylingChar + quoteAuthorElem + lineBreak + via + addOnHandle + related;;
     //generate url available for Twitter intent and inject url on HTML
     document.getElementById('tweet-quote').href = "https://twitter.com/intent/tweet?text=" + subString;
   } else {
     //generate url available for Twitter intent and inject url on HTML
-    document.getElementById('tweet-quote').href = "https://twitter.com/intent/tweet?text=" + contentQuote;
+    document.getElementById('tweet-quote').href = "https://twitter.com/intent/tweet?text=" + contentQuote + related;
   } 
 };
 
